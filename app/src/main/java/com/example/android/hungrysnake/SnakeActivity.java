536070cb72b7de157bc5cb1610ae6ebde5b456a0
mikeweishuai <shuaiw4@illinois.edu>
2018-11-14 18:32:16 -0600

    created new activity

--- app/src/main/java/com/example/android/hungrysnake/SnakeActivity.java
+++ app/src/main/java/com/example/android/hungrysnake/SnakeActivity.java
@@ -2,15 +2,12 @@ package com.example.android.hungrysnake;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-    int a = 0;
-    private Button toSnakeView;
+public class SnakeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_snake);
     }
 }
