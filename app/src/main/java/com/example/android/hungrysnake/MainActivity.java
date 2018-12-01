6604cf4a47d4042e22b0001d40fb6462b21022a2
mikeweishuai <shuaiw4@illinois.edu>
2018-12-01 14:54:53 -0600

    finished basic function

--- app/src/main/java/com/example/android/hungrysnake/MainActivity.java
+++ app/src/main/java/com/example/android/hungrysnake/MainActivity.java
@@ -1,16 +1,22 @@
 package com.example.android.hungrysnake;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Button;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
-    int a = 0;
-    private Button toSnakeView;
+
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void openSnakeGame(View view) {
+        Intent i = new Intent(this, SnakeActivity.class);
+        startActivity(i);
     }
 }
--- app/src/main/java/com/example/android/hungrysnake/SnakeActivity.java
+++ app/src/main/java/com/example/android/hungrysnake/SnakeActivity.java
@@ -1,13 +1,45 @@
 package com.example.android.hungrysnake;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.graphics.Point;
+import android.os.Bundle;
+import android.view.Display;
 
-public class SnakeActivity extends AppCompatActivity {
+public class SnakeActivity extends Activity {
+
+    SnakeEngine mSnakeEngine;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_snake);
+
+        // Get the pixel dimensions of the screen
+        Display display = getWindowManager().getDefaultDisplay();
+
+        // Initialize the result into a Point object
+        Point size = new Point();
+        display.getSize(size);
+
+        // Create a new instance of the SnakeEngine class
+        mSnakeEngine = new SnakeEngine(this, size);
+
+        // Make snakeEngine the view of the Activity
+        setContentView(mSnakeEngine);
+    }
+
+    // Start the thread in snakeEngine
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mSnakeEngine.resume();
+    }
+
+    // Stop the thread in snakeEngine
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mSnakeEngine.pause();
     }
 }
